c0f1dcfa42dd7e6c0ff097bedc08c4bce77a7948
Vladimir <vovchikzaharov1991@gmail.com>
2022-04-27 17:02:08 +0300

    feat: completed the task hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,11 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed ) {
+  //Mn = 2^n - 1.
+  let countSteps = Math.pow(2, disksNumber) - 1
+  console.log(countSteps);
+  //t = Mn / s.
+  let countSeconds = Math.floor(countSteps / (turnsSpeed / 3600)) ;
+  console.log(countSeconds);
+  return {
+    turns: countSteps, 
+    seconds: countSeconds
+  };
 }
 
 module.exports = {
   calculateHanoi
 };
+// calculateHanoi(9, 4308);
