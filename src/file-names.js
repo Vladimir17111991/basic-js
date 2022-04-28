8e4b072456f2ba8a9a651a6a0b57b9b4a88c0da2
Vladimir <vovchikzaharov1991@gmail.com>
2022-04-28 19:33:15 +0300

    feat: completed the task file-names

--- src/file-names.js
+++ src/file-names.js
@@ -15,11 +15,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  
+if(names.length === 0){return [];}
+let array = [];
+let count = 1;
+// let obj = {};
+for(let i = 0; i < names.length; i++){
+  // console.log(names.indexOf(names[i]) + " - " + names[i])
+  if(array.includes(names[i])){
+    // names[i] = names[i] + `(${count})`;
+    names[i] = names[i] + `(${names.includes(names.indexOf(names[i])) + 1 +count})`;
+    count++;
+    array.push(names[i]);
+  } else{
+    array.push(names[i])
+    count=0;
+  }
+}
+// console.log(array)
+return array;
+
 }
 
 module.exports = {
   renameFiles
 };
+// renameFiles(['doc', 'doc', 'image', 'doc(1)', 'doc'])  // ['doc', 'doc(1)', 'image', 'doc(1)(1)', 'doc(2)']
+// renameFiles(['a', 'b', 'cd', 'b ', 'a(3)'])           // ['a', 'b', 'cd', 'b ', 'a(3)']
+renameFiles([]); //[]
